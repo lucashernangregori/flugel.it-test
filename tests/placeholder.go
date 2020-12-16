cbf026682d93b4849a3aaa0fef241a6cb707d754
Lucas Hernan Gregori <lucas.hernan.gregori@gmail.com>
2020-12-16 03:14:00 -0300

    comment compliance rule for linter

--- tests/placeholder.go
+++ tests/placeholder.go
@@ -1,4 +1,4 @@
 package test
 
-//this is a placeholder so go get -v won't fail with an exit code of 1 (because it does that if there is no non *_test.go files)
-//https://stackoverflow.com/questions/46899112/go-build-error-no-non-test-go-files-in-dir
+// this is a placeholder so go get -v won't fail with an exit code of 1 (because it does that if there is no non *_test.go files)
+// https://stackoverflow.com/questions/46899112/go-build-error-no-non-test-go-files-in-dir

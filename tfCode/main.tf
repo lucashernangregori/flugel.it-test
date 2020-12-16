3869076b54d8c7ff62df1c22fb7d08221088a947
Lucas Hernan Gregori <lucas.hernan.gregori@gmail.com>
2020-12-16 01:24:52 -0300

    super linter added to github actions

--- tfCode/main.tf
+++ tfCode/main.tf
@@ -6,11 +6,24 @@ terraform {
 resource "aws_s3_bucket" "first_bucket" {
   provider = aws.region_master
   bucket   = var.bucket_name
-  acl      = "private"
 
+  versioning {
+    enabled = true
+  }
   tags = {
     Name = "flugel.it challenge"
   }
+}
+
+//resource created to conform super linter false positive: https://github.com/accurics/terrascan/issues/359
+resource "aws_s3_bucket_public_access_block" "first_bucket" {
+  provider = aws.region_master
+  bucket = aws_s3_bucket.first_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_object" "test_files" {

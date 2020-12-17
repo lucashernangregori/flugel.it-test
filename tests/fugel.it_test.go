5d025e10983193630eaf04afb139d7ade825a7ea
root <root@DESKTOP-F5RPDGC.localdomain>
2020-12-17 06:11:26 -0300

    changing import syntax

--- tests/fugel.it_test.go
+++ tests/fugel.it_test.go
@@ -5,14 +5,14 @@ import (
 	"strings"
 	"testing"
 
-	// "github.com/gruntwork-io/terratest/modules/aws"
-	// "github.com/gruntwork-io/terratest/modules/random"
-	// "github.com/gruntwork-io/terratest/modules/terraform"
-	// "github.com/stretchr/testify/assert"
-	"github.com/gruntwork-io/terratest@v0.31.1/modules/aws"
-	"github.com/gruntwork-io/terratest@v0.31.1/modules/random"
-	"github.com/gruntwork-io/terratest@v0.31.1/modules/terraform"
-	"github.com/stretchr/testify@v1.4.0/assert"
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+	// "github.com/gruntwork-io/terratest@v0.31.1/modules/aws"
+	// "github.com/gruntwork-io/terratest@v0.31.1/modules/random"
+	// "github.com/gruntwork-io/terratest@v0.31.1/modules/terraform"
+	// "github.com/stretchr/testify@v1.4.0/assert"
 )
 
 func TestTerraformCode(t *testing.T) {

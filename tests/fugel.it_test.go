3caa2b9f7dd37389bd0fb8616f641c50b02c0d9a
infernalkiller69 <76224899+infernalkiller69@users.noreply.github.com>
2020-12-18 16:02:22 -0300

    Update fugel.it_test.go

--- tests/fugel.it_test.go
+++ tests/fugel.it_test.go
@@ -14,21 +14,13 @@ import (
 )
 
 func TestTerraformCode(t *testing.T) {
-	fmt.Print("hola")
+	fmt.Print("secreto:")
 	file, err := os.Open("test2")
     		if err != nil {
         	log.Fatal(err)
     	}
 
-    	fmt.Print(file)
-	
-	fmt.Println("Shell:", os.Getenv("MY_SECRET"))
-	fmt.Println("Shell:", os.Getenv("AWS_SECRET_ACCESS_KEY"))
-	for _, e := range os.Environ() {
-		pair := strings.SplitN(e, "=", 2)
-		fmt.Printf("%s: %s\n", pair[0], pair[1])
-    	}
-	
+    	
 	t.Parallel()
 	
 	expectedBucketName := fmt.Sprintf("flugel.it.lucashernangregori.com.terratest-%s", strings.ToLower(random.UniqueId()))
